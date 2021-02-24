33b788c6a38134d6cd0568b2992baa703ac5acdc
Emil Bækdahl <ebakda16@student.aau.dk>
2021-02-24 14:03:43 +0100

    Fix Roxygen typos and add namespaces to util.R

--- pykg2vec-grid-search/analysis/src/util.R
+++ pykg2vec-grid-search/analysis/src/util.R
@@ -1,31 +1,31 @@
 #' Loads a single data frame.
-#' 
+#'
 #' @param dataset Name of the dataset to load.
 #' @param model Name of the model to load.
 #' @param base_path Root of the data folder.
-#' 
-#' @returns Data frame.
+#'
+#' @return Data frame.
 #' @export
-#' 
-#' @example 
+#'
+#' @examples
 #' load_data("wn", "TransE", base_path = "path/to/data")
-load_data <- function(dataset, model, base_path = "data") 
-  str_c(base_path, dataset, model, sep = "/") %>% 
-  str_c(".csv") %>%
-  readr::read_csv() %>% 
-  select(-device, -sampling, -epochs)
+load_data <- function(dataset, model, base_path = "data")
+  stringr::str_c(base_path, dataset, model, sep = "/") %>%
+  stringr::str_c(".csv") %>%
+  readr::read_csv() %>%
+  dplyr::select(-device, -sampling, -epochs)
 
 #' Loads multiple data frames and combines them into one ready for analysis.
 #'
 #' @param datasets Vector of dataset names.
 #' @param models Vector of model names.
-#' 
-#' @returns Data frame.
+#'
+#' @return Data frame.
 #' @export
-#' 
-#' @examples 
+#'
+#' @examples
 #' load_all_data(c("wn", "fb"), c("TransE", TransH"), base_path = "path/to/data")
-load_all_data <- function(datasets, models, ...) 
-  tidyr::expand_grid(dataset = datasets, model = models) %>% 
-  purrr::pmap(load_data, ...) %>% 
+load_all_data <- function(datasets, models, ...)
+  tidyr::expand_grid(datasets, models) %>%
+  purrr::pmap(load_data, ...) %>%
   dplyr::bind_rows()
